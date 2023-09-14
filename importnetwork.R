024ef6f7a55e1eb5eef09395bcfe769e2369d3eb
pklemmer <klemmerpierre@gmail.com>
2023-09-14 11:53:39 +0200

    fixed filter, overlay WIP

--- importnetwork.R
+++ importnetwork.R
@@ -1,17 +1,14 @@
 sessionInfo()
+setwd("~/GitHub/SCZ-CNV")
 install.packages("BiocManager")
 BiocManager::install("RCy3")
-n
- #'n' added to automatically deny updates in all BiocManager packages
- #'Maybe refine this as probably not all packages are required - takes a few minutes to get started
 BiocManager::install("rWikiPathways")
-n
 #Installing packages
 library(RCy3)
 library(rWikiPathways)
 library(dplyr)
-  #Opening libraries that this script depends on 
-setwd("~/GitHub/SCZ-CNV")
+  #Opening libraries this script depends on 
+
 cytoscapePing()
 cytoscapeVersionInfo()
   #Checking if Cytoscape is running and version info
@@ -22,42 +19,41 @@ installApp('CyTargetLinker')
 scz_pathways <- findPathwaysByText("Schizophrenia")
   #Querying WikiPathways for relevant pathways using "Schizophrenia" as keyword
 scz_pathways <- scz_pathways %>%
-    dplyr::filter(species == c("Homo sapiens","Rattus norvegicus","Mus musculus"))
+  dplyr::filter(species %in% c("Homo sapiens", "Rattus norvegicus", "Mus musculus"))
   #Filtering by species
 scz_pathways.ids <- scz_pathways$id
   #Selecting the WP IDs of the relevant pathways
 
 adc_pathways <- findPathwaysByText("Addiction")
 adc_pathways <- adc_pathways %>%
-  dplyr::filter(species == c("Homo sapiens", "Rattus norvegicus", "Mus musculus"))
+  dplyr::filter(species %in% c("Homo sapiens", "Rattus norvegicus", "Mus musculus"))
 adc_pathways.ids <- adc_pathways$id
-  #Finding and importing addiction pathways 
+  #Finding addiction pathways 
 
-#Filter doesn't work for SCZ if mouse is added but works fine for adc pathways ??
-#Filter works but somehow doesn't extract all the pathways it could
-
-import <- function(i) {commandsRun(paste0('wikipathways import-as-network id=', i))
+import <- function(i) 
+  {commandsRun(paste0('wikipathways import-as-network id=', i))
 }
 
 lapply(scz_pathways.ids,import)
 lapply(adc_pathways.ids,import)  
   #Opening all the pathways previously selected consecutively - probably not so great if there are a lot of pathways
-  #Order is determined by the WP query, in which results are listed according to match scores
 
 scz_pathways.names <- scz_pathways$name
 adc_pathways.names <- adc_pathways$name
 
-intersect_matrix <- expand.grid(scz_pathways.names, adc_pathways.names)
+intersect_df <- expand.grid(scz_pathways.names, adc_pathways.names)
   #Matching every SCZ pathway against every addiction pathway to detect all interactions
 
-merge.intersect <- function(j) {
-  col1 <- j[,1]
-  col2 <- j[,2]
-  intersection <- mergeNetworks(c("col1","col2"), "test", operation = "intersection")
+
+intersect <- function(j) {
+  mergeNetworks(c(j[,1],j[,2]), "test","intersection")
 }
 
-intersections <- merge.intersect(intersect_matrix)
 
+a <- as.character(intersect_df[1,1])
+b <- as.character(intersect_df[2,1])
 
+mergeNetworks(c(a,b),"test","intersection")
+mergeNetworks(c("Disruption of postsynaptic signaling by CNV - Homo sapiens","Opioid receptor pathways - Homo sapiens"),"test")
   #overlay all addiction pathways with all scz pathways and extract interactions
   #group networks together? tidier and easier for overlapping
